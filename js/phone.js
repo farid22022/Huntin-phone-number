18d6695b5d6f09aa6d80e0f2ee43d800ceed3c1f
Farid <mdfaridhossenrehad@gmail.com>
2024-03-02 12:45:56 +0600

    search option added

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async() => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+const loadPhone = async(searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     const data = await res.json();
     const phones = data.data
     // console.log(phones)
@@ -8,13 +8,16 @@ const loadPhone = async() => {
 
 const displayPhones = (phones) =>{
 
-    const phoneContainer = document.getElementById('phone-container')
+    const phoneContainer = document.getElementById('phone-container');
+
+    // clear phone container cards before adding new cards
+    phoneContainer.textContent = ' ';
 
     phones.forEach(phone => {
         console.log(phone);
         //2 : create a div
         const phoneCard = document.createElement('div');
-        phoneCard.classList = 'card w-96 bg-gray-100 shadow-xl';
+        phoneCard.classList = 'card bg-gray-100 p-4  shadow-xl';
 
         //3 :set innerHTML
         phoneCard.innerHTML =`
@@ -32,4 +35,12 @@ const displayPhones = (phones) =>{
     })
 }
 
-loadPhone()
+// handle search button
+const handleSearch = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText);
+}
+// loadPhone();
+
